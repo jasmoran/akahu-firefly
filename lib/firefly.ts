86300c1c833e1d0474d9cab6e3a1ade1b688553c
Jack Moran <jack@earth.co.nz>
2023-01-31 20:43:32 +1300

    Add method for detching Firefly accounts with external IDs

--- lib/firefly.ts
+++ lib/firefly.ts
@@ -7,18 +7,31 @@ export interface AccountWithNumber {
   account_number: string
 }
 
-// Fetch all accounts that have an account_number loaded
-export async function accountsWithNumber (): Promise<AccountWithNumber[]> {
-  const db = knex(firefly)
-  const accounts = await db('accounts')
-    .select('accounts.id', 'accounts.account_type_id', 'account_meta.data')
-    .innerJoin('account_meta', 'accounts.id', 'account_meta.account_id')
-    .where('account_meta.name', 'account_number')
-
-  accounts.forEach(account => {
-    account.account_number = JSON.parse(account.data)
-    delete account.data
-  })
-
-  return accounts
+export interface AccountWithExternalId {
+  id: number
+  account_type_id: number
+  external_id: string
 }
+
+function accountMeta<T> (name: string) {
+  return async function (): Promise<T[]> {
+    const db = knex(firefly)
+    const accounts = await db('accounts')
+      .select('accounts.id', 'accounts.account_type_id', 'account_meta.data')
+      .innerJoin('account_meta', 'accounts.id', 'account_meta.account_id')
+      .where('account_meta.name', name)
+
+    accounts.forEach(account => {
+      account[name] = JSON.parse(account.data)
+      delete account.data
+    })
+
+    return accounts
+  }
+}
+
+// Fetch all accounts that have an account_number loaded
+export const accountsWithNumber = accountMeta<AccountWithNumber>('account_number')
+
+// Fetch all accounts that have an external_id loaded
+export const accountsWithExternalId = accountMeta<AccountWithExternalId>('external_id')
