1227d57cdca4330f1afc198b15eb4adde7bbfde6
Jack Moran <jack@earth.co.nz>
2023-02-20 18:46:29 +1300

    Determine 'other' account when processing Akahu transactions

--- lib/process-transactions.ts
+++ lib/process-transactions.ts
@@ -1,6 +1,6 @@
 import type { Transaction as AkahuTransaction } from 'akahu'
 import Big from 'big.js'
-import { Account, Accounts, AccountType } from './accounts'
+import { Account, AccountPair, Accounts, AccountType } from './accounts'
 import { importAccounts } from './firefly-import'
 import { Transaction, Transactions, TransactionType } from './transactions'
 
@@ -27,6 +27,40 @@ export class ProcessTransactions {
     return processor
   }
 
+  private findAccountPair (transaction: AkahuTransaction): AccountPair {
+    let account: AccountPair | undefined
+
+    // Interest is a special case - match any account that contains the word interest
+    if (transaction.description.toLowerCase().includes('interest')) {
+      account ??= this.accounts.getByName('Interest')
+    }
+
+    // Match account using the Akahu merchant ID
+    if ('merchant' in transaction) {
+      account ??= this.accounts.getByAkahuId(transaction.merchant._id)
+    }
+
+    // Match account using the bank account number
+    if ('meta' in transaction) {
+      account ??= this.accounts.getByBankNumber(transaction.meta.other_account ?? '')
+    }
+
+    // If all else fails match account using fuzzy name matching
+    // Match the description with and without the reference - pick the best result
+    if (account === undefined) {
+      let name = transaction.description
+      let match = this.accounts.getByNameFuzzy(name)
+      if ('meta' in transaction) {
+        name = name.replace(transaction.meta.reference ?? '', '')
+        const newMatch = this.accounts.getByNameFuzzy(name)
+        if (newMatch[1] > match[1]) match = newMatch
+      }
+      account ??= match[0]
+    }
+
+    return account
+  }
+
   public akahuToFirefly (transaction: AkahuTransaction): Transaction {
     // TODO:
     // transaction.meta.reference
@@ -37,29 +71,54 @@ export class ProcessTransactions {
     // transaction.merchant
 
     // Look up Akahu Account ID (acc_xxxxx)
-    const accountSet = this.accounts.getByAkahuId(transaction._account)
-    if (accountSet === undefined) throw Error(`Akahu account ${transaction._account} not set up in Firefly`)
-    const account = accountSet.asset ?? accountSet.liability
-    if (account === undefined) throw Error(`User's account ${transaction._account} not configured as an asset or liability`)
+    const pair = this.accounts.getByAkahuId(transaction._account)
+    if (pair === undefined) throw Error(`Akahu account ${transaction._account} not set up in Firefly`)
+    const account = pair.source ?? pair.destination
+    if (account === undefined) throw Error('Found invalid AccountPair')
+    if (account.type !== AccountType.Asset && account.type !== AccountType.Liability) throw Error(`User's account ${transaction._account} not configured as an asset or liability`)
 
-    const dummyAccount: Account = {
-      id: 0,
-      fireflyId: 0,
-      akahuId: undefined,
-      name: '',
-      type: AccountType.Expense,
-      bankNumbers: new Set(),
-      alternateNames: new Set()
-    }
+    const findAccount = this.findAccountPair(transaction)
 
-    let type, source: Account, destination
+    let type, source: Account, destination: Account
     if (transaction.amount < 0) {
+      if (findAccount.destination === undefined) {
+        const other = findAccount.source
+        if (other === undefined) throw Error('Found invalid AccountPair')
+
+        // TODO: Enhance with data from this transaction
+        destination = this.accounts.create({
+          fireflyId: other.fireflyId,
+          akahuId: other.akahuId,
+          name: other.name,
+          type: AccountType.Expense,
+          bankNumbers: other.bankNumbers,
+          alternateNames: other.alternateNames
+        })
+      } else {
+        destination = findAccount.destination
+      }
+
       type = TransactionType.Withdrawal
       source = account
-      destination = dummyAccount // TODO - expense account
     } else {
+      if (findAccount.source === undefined) {
+        const other = findAccount.destination
+        if (other === undefined) throw Error('Found invalid AccountPair')
+
+        // TODO: Enhance with data from this transaction
+        source = this.accounts.create({
+          fireflyId: other.fireflyId,
+          akahuId: other.akahuId,
+          name: other.name,
+          type: AccountType.Revenue,
+          bankNumbers: other.bankNumbers,
+          alternateNames: other.alternateNames
+        })
+      } else {
+        source = findAccount.source
+      }
+
       type = TransactionType.Deposit
-      source = dummyAccount // TODO - revenue account
       destination = account
     }
 
