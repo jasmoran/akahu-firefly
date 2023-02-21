1537b9d6f9b13c92edf123e2dd990992dfcfbdda
Jack Moran <jack@earth.co.nz>
2023-02-21 17:34:51 +1300

    Create transformTransaction function

--- lib/akahu-import.ts
+++ lib/akahu-import.ts
@@ -44,6 +44,8 @@ interface Row<T> {
   data: T
 }
 
+type IncompleteTransaction = Omit<Transaction, 'id'>
+
 function findAccountPair (accounts: Accounts, transaction: AkahuTransaction): AccountPair {
   let account: AccountPair | undefined
 
@@ -78,99 +80,103 @@ function findAccountPair (accounts: Accounts, transaction: AkahuTransaction): Ac
   return account
 }
 
+function transformTransaction (accounts: Accounts, transaction: AkahuTransaction): IncompleteTransaction {
+  // TODO:
+  // transaction.meta.reference
+  // transaction.meta.particulars
+  // transaction.meta.code
+  // transaction.type
+
+  // Look up Akahu Account ID (acc_xxxxx)
+  const pair = accounts.getByAkahuId(transaction._account)
+  if (pair === undefined) throw Error(`Akahu account ${transaction._account} not set up`)
+  const account = pair.source ?? pair.destination
+  if (account === undefined) throw Error('Found invalid AccountPair')
+  if (account.type !== AccountType.Asset && account.type !== AccountType.Liability) throw Error(`User's account ${transaction._account} not configured as an asset or liability`)
+
+  const findAccount = findAccountPair(accounts, transaction)
+
+  let source: Account, destination: Account
+  if (transaction.amount < 0) {
+    if (findAccount.destination === undefined) {
+      const other = findAccount.source
+      if (other === undefined) throw Error('Found invalid AccountPair')
+
+      // TODO: Enhance with data from this transaction
+      destination = accounts.create({
+        fireflyId: other.fireflyId,
+        akahuId: other.akahuId,
+        name: other.name,
+        type: AccountType.Expense,
+        bankNumbers: other.bankNumbers,
+        alternateNames: other.alternateNames
+      })
+    } else {
+      destination = findAccount.destination
+    }
+
+    source = account
+  } else {
+    if (findAccount.source === undefined) {
+      const other = findAccount.destination
+      if (other === undefined) throw Error('Found invalid AccountPair')
+
+      // TODO: Enhance with data from this transaction
+      source = accounts.create({
+        fireflyId: other.fireflyId,
+        akahuId: other.akahuId,
+        name: other.name,
+        type: AccountType.Revenue,
+        bankNumbers: other.bankNumbers,
+        alternateNames: other.alternateNames
+      })
+    } else {
+      source = findAccount.source
+    }
+
+    destination = account
+  }
+
+  const type = transactionMapping[source.type][destination.type]
+  if (type === undefined) throw Error(`Invalid transaction type ${source.type} -> ${destination.type}`)
+
+  const newTrans: IncompleteTransaction = {
+    fireflyId: undefined,
+    akahuIds: new Set([transaction._id]),
+    type,
+    source,
+    destination,
+    date: new Date(transaction.date),
+    amount: Big(transaction.amount).abs(),
+    description: transaction.description
+  }
+
+  // Add foreign currency details if any available
+  if ('meta' in transaction) {
+    const conversion: CurrencyConversion | undefined = (transaction.meta.conversion as unknown) as CurrencyConversion | undefined
+    if (conversion !== undefined) {
+      newTrans.foreignAmount = Big(conversion.amount).abs()
+      newTrans.foreignCurrencyCode = conversion.currency
+      // TODO: Store fee/rate
+    }
+  }
+
+  // Use personal finance group as category
+  if ('category' in transaction) {
+    const categoryName = transaction.category.groups?.['personal_finance']?.name
+    if (categoryName !== undefined) newTrans.categoryName = categoryName
+    // TODO: Store other categories
+  }
+
+  return newTrans
+}
+
 export async function importTransactions (accounts: Accounts, transactions: Transactions): Promise<void> {
   const db = knex(production)
   const transactionsTable = db<Row<AkahuTransaction>, any>('akahu_transactions')
   const akahuTransactions = await transactionsTable.pluck('data')
 
   akahuTransactions.forEach(transaction => {
-    // TODO:
-    // transaction.meta.reference
-    // transaction.meta.particulars
-    // transaction.meta.code
-    // transaction.type
-
-    // Look up Akahu Account ID (acc_xxxxx)
-    const pair = accounts.getByAkahuId(transaction._account)
-    if (pair === undefined) throw Error(`Akahu account ${transaction._account} not set up in Firefly`)
-    const account = pair.source ?? pair.destination
-    if (account === undefined) throw Error('Found invalid AccountPair')
-    if (account.type !== AccountType.Asset && account.type !== AccountType.Liability) throw Error(`User's account ${transaction._account} not configured as an asset or liability`)
-
-    const findAccount = findAccountPair(accounts, transaction)
-
-    let source: Account, destination: Account
-    if (transaction.amount < 0) {
-      if (findAccount.destination === undefined) {
-        const other = findAccount.source
-        if (other === undefined) throw Error('Found invalid AccountPair')
-
-        // TODO: Enhance with data from this transaction
-        destination = accounts.create({
-          fireflyId: other.fireflyId,
-          akahuId: other.akahuId,
-          name: other.name,
-          type: AccountType.Expense,
-          bankNumbers: other.bankNumbers,
-          alternateNames: other.alternateNames
-        })
-      } else {
-        destination = findAccount.destination
-      }
-
-      source = account
-    } else {
-      if (findAccount.source === undefined) {
-        const other = findAccount.destination
-        if (other === undefined) throw Error('Found invalid AccountPair')
-
-        // TODO: Enhance with data from this transaction
-        source = accounts.create({
-          fireflyId: other.fireflyId,
-          akahuId: other.akahuId,
-          name: other.name,
-          type: AccountType.Revenue,
-          bankNumbers: other.bankNumbers,
-          alternateNames: other.alternateNames
-        })
-      } else {
-        source = findAccount.source
-      }
-
-      destination = account
-    }
-
-    const type = transactionMapping[source.type][destination.type]
-    if (type === undefined) throw Error(`Invalid transaction type ${source.type} -> ${destination.type}`)
-
-    const fireflyTrans: Omit<Transaction, 'id'> = {
-      fireflyId: undefined,
-      akahuIds: new Set([transaction._id]),
-      type,
-      source,
-      destination,
-      date: new Date(transaction.date),
-      amount: Big(transaction.amount).abs(),
-      description: transaction.description
-    }
-
-    // Add foreign currency details if any available
-    if ('meta' in transaction) {
-      const conversion: CurrencyConversion | undefined = (transaction.meta.conversion as unknown) as CurrencyConversion | undefined
-      if (conversion !== undefined) {
-        fireflyTrans.foreignAmount = Big(conversion.amount).abs()
-        fireflyTrans.foreignCurrencyCode = conversion.currency
-        // TODO: Store fee/rate
-      }
-    }
-
-    // Use personal finance group as category
-    if ('category' in transaction) {
-      const categoryName = transaction.category.groups?.['personal_finance']?.name
-      if (categoryName !== undefined) fireflyTrans.categoryName = categoryName
-      // TODO: Store other categories
-    }
-
-    transactions.create(fireflyTrans)
+    transactions.create(transformTransaction(accounts, transaction))
   })
 }
