e510251c108e00cc31df90fb9c1bc5754bb21e89
Jack Moran <jack@earth.co.nz>
2023-02-24 14:01:20 +1300

    Merge Akahu transactions into Firefly transactions

--- lib/process-transactions.ts
+++ lib/process-transactions.ts
@@ -3,9 +3,20 @@ import * as akahuImport from './akahu-import'
 
 export class ProcessTransactions {
   public async processTransactions (): Promise<void> {
+    console.log('Importing Firefly accounts')
     const fireflyAccounts = await fireflyImport.importAccounts()
+
+    console.log('Importing Firefly transactions')
     const fireflyTransactions = await fireflyImport.importTransactions(fireflyAccounts)
 
+    console.log('Importing Akahu transactions')
     const akahuTransactions = await akahuImport.importTransactions(fireflyAccounts)
+
+    console.log('Merging transactions')
+    const mergedTransactions = fireflyTransactions.duplicate()
+    mergedTransactions.merge(akahuTransactions, (a, b) => {
+      // Check Akahu IDs match
+      return [...a.akahuIds].sort().join(',') === [...b.akahuIds].sort().join(',')
+    })
   }
 }
